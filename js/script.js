a46824952ff8dd03559715c933f5642fb79cc59c
kerdaino <kerdaino3@gmail.com>
2023-05-25 09:21:00 -0700

    third-commit

--- js/script.js
+++ js/script.js
@@ -11,10 +11,14 @@ const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
 
-addGuestButton.addEventListener("click",function(){
+const assignButton = document.querySelector(".assign");
+
+const assignedItems = document.querySelector(".assigned-items");
+
+addGuestButton.addEventListener("click", function () {
     const guest = guestInput.value;
     // console.log(guest);
-    if(guest !== ""){
+    if (guest !== "") {
         // let listItem = document.createElement("li");
         // listItem.innerText = guest;
         // guestList.append(listItem);
@@ -24,23 +28,48 @@ addGuestButton.addEventListener("click",function(){
     }
 });
 
-const clearInput = function(){
+const clearInput = function () {
     guestInput.value = "";
 };
 
-const addToList = function(guest){
+const addToList = function (guest) {
     const listItem = document.createElement("li");
-        listItem.innerText = guest;
-        guestList.append(listItem);
+    listItem.innerText = guest;
+    guestList.append(listItem);
 };
 
-const updateGuestCount = function(){
+const updateGuestCount = function () {
     const guests = document.querySelectorAll(".guest-list li");
     guestCount.innerText = "attendance".length;
-    if(guests.length === 8){
+    if (guests.length === 8) {
         addGuestButton.classList.add("hide");
         guestInput.classList.add("hide");
         guestInputLabel.classList.add("hide");
         guestFull.classList.remove("hide");
     }
 };
+
+const assignItems = function () {
+    const potluckItems = ["Halloumi and MeLON Skewers",
+    "Peach and Prosciutto bruschetta", 
+    "veggie nori rolls", 
+    "tomato and mozzarella caprese skewers", 
+    "stuffed peppadews with parmesan & salami", 
+    "3 - cheese tomato tart", 
+    "zucchini noodles with tomatoes and corn",
+"fish and meat" ];
+    const allGuests = document.querySelectorAll(".guest-list li");
+    for(let guest of allGuests){
+        let randomPotluckIndex = Math.floor(Math.random()*potluckItems.length);
+        let randomPotluckItem = potluckItems[randomPotluckIndex];
+        let listItem = document.createElement("li");
+        listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+        assignedItems.append(listItem);
+        potluckItems.splice(randomPotluckIndex,1);
+    };
+};
+
+assignButton.addEventListener("click",function(){
+    assignItems();
+    assignButton.disabled = true;
+});
